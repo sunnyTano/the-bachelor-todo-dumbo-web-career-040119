15e80e9d8619076112806e4779a88a974d1a984b
Susan Tan <ssusantan9@gmail.com>
2019-04-02 13:02:46 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,26 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+     binding.pry 
+ data[season].each do |k, v|
+    puts "#{k}"
+end
 end
 
-def get_contestant_name(data, occupation)
-  # code here
-end
+puts get_first_name_of_season_winner
 
-def count_contestants_by_hometown(data, hometown)
-  # code here
-end
+# def get_contestant_name(data, occupation)
+#   # code here
+# end
 
-def get_occupation(data, hometown)
-  # code here
-end
+# def count_contestants_by_hometown(data, hometown)
+#   # code here
+# end
 
-def get_average_age_for_season(data, season)
-  # code here
-end
+# def get_occupation(data, hometown)
+#   # code here
+# end
+
+# def get_average_age_for_season(data, season)
+#   # code here
+# end
